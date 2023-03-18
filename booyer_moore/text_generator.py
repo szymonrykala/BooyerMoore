8c1fae54b1e91cde9f8be3dab5d238a96ad19882
Jakub Olech <24255215+BlayeeR@users.noreply.github.com>
2023-03-18 15:30:40 +0100

    Reimplement random function

--- booyer_moore/text_generator.py
+++ booyer_moore/text_generator.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 
 from concurrent.futures import ThreadPoolExecutor
-import random
 import sys
 import string
 from typing import Iterator
+import numpy as np
 
 from utils import get_arg
-
-def offset(array: list[int], offset = 1) -> list[int]:
-    return [z - min(array) + offset for z in array] if min(array) < 1 else array # no need for ofset if we have positive numbers
 
 def generate_text(length: int, mean: float, std_dev: float, character_set: str):
     mean = (len(character_set) - 1) / 2 if not mean else mean
     std_dev = mean / 2 if not std_dev else std_dev
 
-    # random.choices weights sum needs to be greater than 0, so offset them so its always the case
-    weights = offset([random.normalvariate(mean, std_dev) for _ in range(len(character_set))])
+    sample = ""
 
-    sample = random.choices(
-        character_set,
-        weights=weights,
-        k=length
-    )
+    for _ in range(length):
+        character_weights = np.random.normal(mean, std_dev, len(character_set))
+        sample += character_set[character_weights.argmax()]
 
     return ''.join(sample)
 
@@ -45,7 +39,7 @@ if __name__ == "__main__":
 
     MEAN = float(get_arg("--mean", 0))
     STD_DEV = float(get_arg("--std-dev", 0))
-    CHARACTER_SET = get_arg("--character-set", string.ascii_letters + ' ')
+    CHARACTER_SET = get_arg("--character-set", string.ascii_lowercase + ' ')
     WORKERS = int(get_arg("--workers", 1)) or 1
 
     length_per_worker = round(LENGTH/WORKERS)
