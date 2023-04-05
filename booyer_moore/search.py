bfdb2f319316e7a962cbed1cb7b4d0ec78a9499c
Szymon Rykała <szymon.rykala@accenture.com>
2023-04-05 10:06:39 +0200

    clear the code

--- booyer_moore/search.py
+++ booyer_moore/search.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 
-from concurrent.futures import ThreadPoolExecutor
-from dataclasses import dataclass
 from time import time
 import sys
 from utils import get_arg
-
-
-@dataclass
-class Match:
-    start: int
-    end: int
-    text: str
-
-
-@dataclass
-class Segment:
-    start: int
-    text: str
 
 
 def get_text():
@@ -28,122 +13,50 @@ def get_text():
             return stream.read()
 
 
-def split_to_segments(text: str, pattern: str, length: int = 100):
-
-    offset = 0
-    for i in range(0, len(text), length):
-        seg = Segment(i, text[i+offset: length + i+offset])
-
-        while seg.text and seg.text[-1] in pattern:
-            offset += 1
-            print('correct')
-            seg = Segment(i, text[i: length + i + offset])
-
-        yield seg
+NO_OF_CHARS = 256
 
 
-NO_OF_CHARS = 256000
+def __pattern_matching_chars_map(string, size):
+    pattern_chars_map = [-1]*NO_OF_CHARS
 
-
-def badCharHeuristic(string, size):
-    '''
-    The preprocessing function for
-    Boyer Moore's bad character heuristic
-    '''
-
-    # Initialize all occurrence as -1
-    badChar = [-1]*NO_OF_CHARS
-
-    # Fill the actual value of last occurrence
     for i in range(size):
-        badChar[ord(string[i])] = i
+        pattern_chars_map[ord(string[i])] = i
 
-    # return initialized list
-    return badChar
+    return pattern_chars_map
 
 
-def search(txt, pat):
+def search(txt, pattern):
     results = []
-    pattern_length = len(pat)
+    pattern_length = len(pattern)
     text_length = len(txt)
 
-    badChar = badCharHeuristic(pat, pattern_length)
+    pattern_chars = __pattern_matching_chars_map(pattern, pattern_length)
 
-    # s is shift of the pattern with respect to text
-    pattern_shift = 0
-    while (pattern_shift <= text_length-pattern_length):
-        j = pattern_length-1
-
-        # Keep reducing index j of pattern while
-        # characters of pattern and text are matching
-        # at this shift s
-        while j >= 0 and pat[j] == txt[pattern_shift+j]:
+    cursor_position = 0
+    while (cursor_position <= text_length - pattern_length):
+        j = pattern_length - 1
+        print(f"{cursor_position}")
+        while j >= 0 and pattern[j] == txt[cursor_position + j]:
             j -= 1
 
-        # If the pattern is present at current shift,
-        # then index j will become -1 after the above loop
         if j < 0:
-            start = pattern_shift
-            end = start + len(pat)
-            results.append(Match(start, end, txt[start:end]))
+            results.append((
+                cursor_position,
+                cursor_position + pattern_length
+            ))
 
-            pattern_shift += (pattern_length - badChar[ ord( txt[pattern_shift + pattern_length] ) ]
-                              if pattern_shift + pattern_length < text_length else 1)
-            # print(f"pattern_shift={pattern_shift}")
+            if cursor_position + pattern_length < text_length:
+                cursor_position += (pattern_length -
+                                  pattern_chars[ord(txt[cursor_position + pattern_length])])
+            else:
+                cursor_position += 1
+
         else:
-            pattern_shift += max(1, j-badChar[ord(txt[pattern_shift+j])])
-    return results
-
-
-def align_pattern(pattern: str, text: str, i: int):
-    text[i] == pattern
-    start = i
-    end = i + len(pattern)
-    print(i)
-    for occ in range(1, round((pattern.count(text[i])-1)/2)):
-        print(f"occ={occ}")
-        print(f"i-occ = {text[i-occ]}")
-
-        # if text[i-occ]:
-
-        if i+occ <= len(text)-1:
-            print(f"i+occ = {text[i+occ]}")
-
-        # for j in range(i-len(pattern), i+len(pattern)):
-        #     print(text[j])
-        # if text[i+1] in pattern:
-
-    print("", end="\n")
-
-    # for j in range(1, round(len(pattern)/2)):
-    #     inj = patterntext[i+j]
-
-
-def BMsearch(seg: Segment, pattern: str):
-    results = []
-    for i in range(len(pattern)-1, len(seg.text), len(pattern)):
-        # print(f"skok-index i: {i}")
-        # print(f"text[i]: {seg.text[i]}")
-
-        if seg.text[i] in pattern:
-
-            ind = pattern.index(seg.text[i])
-
-            if seg.text[i] == pattern[ind]:
-                start = i - ind
-            # start = i-pattern.index(seg.text[i])
-
-            # align_pattern(pattern, seg.text, i)
-            # jakoś inaczej wyrównywać słowa
-
-            end = start + len(pattern) - 1
-
-            match = seg.text[start: end]
-
-            if (match == pattern):
-                # print("match !",end="\n\n")
-                results.append(
-                    Match(seg.start + start, seg.start + end, match))
+            try:
+                cursor_position += max(1, j-pattern_chars[ord(txt[cursor_position + j])])
+            except IndexError:
+                #Unsupported char
+                cursor_position += 1
 
     return results
 
@@ -152,34 +65,22 @@ if __name__ == "__main__":
     TEXT = get_text()
     PATTERN = get_arg("--pattern")
 
-    SEGMENT_LENGTH = int(get_arg("--segment-length", 0))
-    WORKERS = int(get_arg("--workers", 1)) or 1
-
+    print("-"*20)
     print(f"pattern: {PATTERN}")
-    print(f"segment-length: {SEGMENT_LENGTH}")
-    print(f"workers: {WORKERS}", end="\n\n")
-    print("------")
     print(f"text lenght: {len(TEXT)}")
+    print("-"*20)
 
     start_time = time()
 
-    segments = split_to_segments(TEXT, PATTERN, SEGMENT_LENGTH) if \
-        SEGMENT_LENGTH > 0 else [Segment(0, TEXT)]
+    results = search(TEXT, PATTERN)
 
-    with ThreadPoolExecutor(WORKERS) as exc:
-        results = []
-        search_jobs = [
-            exc.submit(search, txt=seg.text, pat=PATTERN)
-            for seg in segments
-        ]
-
-        for job in search_jobs:
-            results.extend(job.result())
-
-    print(f"work time: {(time()-start_time)*1_000} ms", end="\n\n")
+    end_time = time()-start_time
 
     print("Results:")
-    for r in results:
-        print(r)
+    for start, end in results:
+        print(f"index: {start}:{end}")
 
+    print("-"*20)
+    print(f"work time: {end_time*1_000} ms", end="\n\n")
+    print(f"Pattern: {PATTERN}")
     print(f"Total count: {len(results)}")
