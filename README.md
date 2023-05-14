a3d62bc9ed87f617a7c0f94e5c809432cb0a95fc
Szymon Rykala <szymonrykala@gmail.com>
2023-05-15 00:32:14 +0200

    fix bug and add comments

--- README.md
+++ README.md
@@ -7,8 +7,7 @@
 Available script parameters:
 * `--text-file` - optional - default: stdin
 * `--pattern` - required
-* `--segment-length` - text segment length pushed to worker thread - default: 2000
-* `--segments-per-worker` - number of segments pushed on single worker thread - default: 3
+* `--worker` - number of segments pushed on single worker thread - default: 3
 
 Invoking samples:
 ```bash
@@ -16,7 +15,7 @@ cat text_file.txt | search.py --pattern ale
 
 search.py --text-file text_file.txt --pattern ale
 
-./search.py --text-file ./text.txt --pattern ale --segment-length 6000 --segments-per-worker 8
+./search.py --text-file ./text.txt --pattern ale --worker 8
 ```
 
 ## Text generation script
--- booyer_moore/search.py
+++ booyer_moore/search.py
@@ -12,96 +12,118 @@ def get_text():
     if not sys.stdin.isatty():  # executed with stdin given with "|"
         return sys.stdin.read()
     else:
+        # read the file given on '--text-file' parameter
         with open(get_arg("--text-file"), "r") as stream:
             return stream.read()
 
 
-NO_OF_CHARS = 256
+NUMBER_OF_CHARS = 256 # set of unicode code points
 
 
-def __pattern_matching_chars_map(string, size):
-    pattern_chars_map = [-1] * NO_OF_CHARS
+def __pattern_matching_chars_map(pattern: str):
+    '''
+        builds chars map used to match pattern in the text
+    '''
+    pattern_chars_map = [-1] * NUMBER_OF_CHARS
 
-    for i in range(size):
-        pattern_chars_map[ord(string[i])] = i
+    for position, char in enumerate(pattern):
+        # assign last char position in the pattern to its unicode point
+        pattern_chars_map[ord(char)] = position
 
     return pattern_chars_map
 
 
-def search(txt, pattern) -> Generator[tuple[int, int], None, None]:
+def bm_search(txt:str, pattern: str, text_start_index: int = 0) -> Generator[tuple[int, int],None,None]:
     pattern_length = len(pattern)
     text_length = len(txt)
 
-    pattern_chars = __pattern_matching_chars_map(pattern, pattern_length)
+    pattern_chars = __pattern_matching_chars_map(pattern)
 
     cursor_position = 0
+
+    # go with the cursor through the text
     while cursor_position <= text_length - pattern_length:
-        j = pattern_length - 1
+        char_number = pattern_length - 1
 
-        while j >= 0 and pattern[j] == txt[cursor_position + j]:
-            j -= 1
+        # compare each char of the pattern starting from back
+        while char_number >= 0 and pattern[char_number] == txt[cursor_position + char_number]:
+            # move index to the next letter ( if pattern="lorem" -> from "m" to "e" )
+            char_number -= 1 
 
-        if j < 0:
-            yield (cursor_position, cursor_position + pattern_length)
-
-            if cursor_position + pattern_length < text_length:
-                cursor_position += pattern_length - pattern_chars[ord(txt[cursor_position + pattern_length])]
-            else:
-                cursor_position += 1
+        # if the whole pattern is a match, 'char_number' should be less than 0.
+        # It means that we looped though each char of the pattern
+        if char_number < 0:
+            cursor_end_scope = cursor_position + pattern_length
+            
+            # yield a match indexes in global text context
+            yield (text_start_index + cursor_position, cursor_end_scope + text_start_index)
+            cursor_position += 1 
 
         else:
             try:
-                cursor_position += max(1, j - pattern_chars[ord(txt[cursor_position + j])])
+                # move cursor by the maximum from 1 and last matching 'char_number' - position of that char in the text
+                cursor_position += max(1, char_number - pattern_chars[ord(txt[cursor_position + char_number])])
             except IndexError:
                 # Unsupported char
                 cursor_position += 1
 
 
 if __name__ == "__main__":
+    # reading script parameters
     TEXT = get_text()
     PATTERN = get_arg("--pattern")
-
-    workers = int(get_arg("--workers", 2))
+    workers_count = int(get_arg("--workers", 1))
 
     text_length = len(TEXT)
-    # max_text_size = 38_000_000
-    max_text_size = round(text_length / workers)
+    max_text_size = round(text_length / workers_count)
 
     print("-" * 20)
     print(f"pattern: {PATTERN}")
     print(f"text lenght: {len(TEXT)}")
+    print(f"workers count: {workers_count}")
     print("-" * 20)
 
-    def get_chunked_text(text: str) -> list[str]:
-        l = list(range(1, text_length, max_text_size))
-        fragments = []
-        for i, num in enumerate(l[1:], 1):
-            while text[num] in PATTERN:
-                l[i] += len(PATTERN)
-                num += len(PATTERN)
+    def get_chunked_text(text: str) -> Generator[str, None, None]:        
+        # list of indexes used to split the text  
+        chunk_indexes = list(range( # range
+            0, # from
+            text_length + max_text_size, # to
+            max_text_size # step
+        )) # sample output = [0, 5, 10, 15]
+
+        # enumerate through list of indexes starting from second element
+        for i, text_index in enumerate(chunk_indexes[1:], 1):
+            
+            # until the end letter of the text chunk is present in search pattern
+            while len(text) > text_index and text[text_index] in PATTERN:
+                
+                # expand the chunk
+                text_index = chunk_indexes[i] = text_index + len(PATTERN) # update index in indexes list
             else:
-                fragments.append(text[l[i - 1] : num])
 
-        return fragments
+                # in other case return the text chunk
+                yield text[chunk_indexes[i - 1] : text_index], chunk_indexes[i - 1]
 
-    if text_length > max_text_size:
-        text_list = get_chunked_text(TEXT)
-    else:
-        text_list = (TEXT,)
 
     results = []
-
-    with ThreadPoolExecutor(round(text_length / max_text_size) or 1) as exec:
+    with ThreadPoolExecutor(workers_count) as exec:
         start_time = time()
-        jobs = tuple(exec.submit(search, text_chunk, PATTERN) for text_chunk in text_list)
+        
+        # for each text chunk, call 'bm_search' function to create async jobs
+        jobs = tuple(
+            exec.submit(bm_search, text_chunk, PATTERN, text_start_index) 
+            for text_chunk, text_start_index in get_chunked_text(TEXT)
+        )
 
         for job in jobs:
+            # extend 'results' array with job result
             results.extend(job.result())
 
     end_time = time() - start_time
 
+    # printing the results
     print("Results:")
-    for start, end in results:
+    for start, end in sorted(results):
         print(f"index: {start}:{end}")
 
     print("-" * 20)
